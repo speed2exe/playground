3b09af12cf9df91e0379071869bfb803ffc94f69
Fu Zi Xiang <speed2exe@live.com.sg>
2022-08-13 23:22:54 +0800

    update

---
+++ zig/src/sort.zig
@@ -0,0 +1,6 @@
+// create sort algorithm for generic types
+// quick and merge
+const std = @import("std");
+
+pub fn main() void {
+}
---
+++ zig/src/stream.zig
@@ -0,0 +1,23 @@
+// Goal: create a poc on java stream like programming
+
+const std = @import("std");
+const print = std.debug.print;
+
+pub fn main() void {
+    var a = myType{};
+    a.hello();
+}
+
+const myType = struct {
+    a: ?u8 = null,
+
+    fn hello(self: *myType) void {
+        goodbye(self);
+    }
+
+    fn goodbye(self: *myType) void {
+        _ = self;
+        print("before return from goodbye fn",.{});
+    }
+    
+};
