9c68b89008c347290877919e1fab872181124dab
Fu Zi Xiang <speed2exe@live.com.sg>
2022-08-28 01:00:54 +0800

    added interactive cli example

---
+++ zig/icli/icli.zig
@@ -0,0 +1,43 @@
+const std = @import("std");
+const linux = std.os.linux;
+
+pub fn main() !void {
+    // var buffer: [1]u8 = undefined;
+
+    var tty = try std.fs.openFileAbsolute("/dev/tty", .{});
+
+    try setRaw(tty);
+    defer unsetRaw(tty) catch |err| {
+        std.debug.print("unsetRaw failed, {}", .{err});
+    };
+
+    var tty_reader = tty.reader();
+    while (true) {
+        const byte = tty_reader.readByte() catch unreachable;
+        if (byte == 3) {
+            return;
+        }
+        std.debug.print("byte read: {d}\r\n", .{byte});
+    }
+}
+
+// only works for linux
+// TODO: make this work for other platforms
+fn setRaw(file: std.fs.File) !void {
+    var termios = try std.os.tcgetattr(file.handle);
+
+    termios.iflag &= ~(linux.IGNBRK | linux.BRKINT | linux.PARMRK | linux.ISTRIP | linux.INLCR | linux.IGNCR | linux.ICRNL | linux.IXON);
+    termios.oflag &= ~linux.OPOST;
+    termios.lflag &= ~(linux.ECHO | linux.ECHONL | linux.ICANON | linux.ISIG |linux.IEXTEN);
+    termios.cflag &= ~(linux.CSIZE | linux.PARENB);
+    termios.cflag |= linux.CS8;
+    termios.cc[linux.V.MIN] = 1;
+    termios.cc[linux.V.TIME] = 0;
+
+    try std.os.tcsetattr(file.handle, linux.TCSA.NOW, termios);
+}
+
+fn unsetRaw(file: std.fs.File) !void {
+    var termios = try std.os.tcgetattr(file.handle);
+    try std.os.tcsetattr(file.handle, linux.TCSA.NOW, termios);
+}
