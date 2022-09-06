f448723dd4dfd3ec2941d2e064b43bc7d059e438
Fu Zi Xiang <speed2exe@live.com.sg>
2022-09-07 00:19:37 +0800

    update solution

--- zig/practice/median_of_two_sorted_array/solution.zig
+++ zig/practice/median_of_two_sorted_array/solution.zig
@@ -16,7 +16,7 @@ fn findMedianSortedArray(arr1: []u64, arr2: []u64) u64 { // arr1: [2, 3, 5, 8], 
 
     // pi: partition index
     var pi1 = arr1.len / 2;
-    var pi2 = arr2.len / 2;
+    var pi2 = ((arr1.len + arr2.len) / 2) - pi1;
 
     // change pi1 and pi2 until all elems to the left of pi1 and left of pi2
     // are less than all elems to the right of pi1 and pi2
@@ -30,24 +30,22 @@ fn findMedianSortedArray(arr1: []u64, arr2: []u64) u64 { // arr1: [2, 3, 5, 8], 
         const r_2 = if (pi2 == arr2.len) std.math.maxInt(u64) else arr2[pi2];
 
         if (l_1 > r_2) {
-            const steps_1 = pi1;
-            const steps_2 = (arr2.len - pi2);
-            const steps = std.math.min(steps_1, steps_2) / 2;
+            var steps = pi1;
+            if (steps > 1) { steps /= 2; }
             pi1 -= steps;
             pi2 += steps;
             continue;
         }
 
         if (l_2 > r_1) {
-            const steps_1 = pi2;
-            const steps_2 = (arr1.len - pi1);
-            const steps = std.math.min(steps_1, steps_2) / 2;
+            var steps = (arr1.len - pi1);
+            if (steps > 1) { steps /= 2; }
             pi1 += steps;
             pi2 -= steps;
             continue;
         }
 
-        return if (arr1.len + arr2.len % 2 == 0)
+        return if ((arr1.len + arr2.len) % 2 == 0)
             std.math.max(l_1, l_2)
         else
             std.math.min(r_1, r_2);
@@ -78,5 +76,23 @@ test "findMedianSortArrays" {
         var arr2 = [_]u64{4, 5, 8, 9};
         try testing.expectEqual(@as(u64, 7), findMedianSortedArray(&arr1, &arr2));
         // 6.5 is the correct answer, but we're rounding down.
+    }
+
+    {
+        // 1 to 50 sorted
+        var arr1 = [_]u64 {
+            11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+            21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
+
+            31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+            41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+            51, 52, 53, 54, 55, 56, 57, 58, 59, 60
+        };
+
+        var arr2 = [_]u64 {
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+        };
+
+        try testing.expectEqual(@as(u64, 30), findMedianSortedArray(&arr1, &arr2));
     }
 }
