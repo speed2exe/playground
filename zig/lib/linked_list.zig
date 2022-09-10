1d091f0a71fd37be2d3aee46f338d09dfe421488
Fu Zi Xiang <speed2exe@live.com.sg>
2022-09-10 21:44:05 +0800

    refactor

--- zig/lib/linked_list.zig
+++ zig/lib/linked_list.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const testing = std.testing;
 const warn = std.log.warn;
+var k: bool = false;
 
 pub fn FixedQueue (
     comptime T: type,
@@ -22,12 +23,15 @@ pub fn FixedQueue (
         tail_ptr: ?*Node(T) = null,
 
         // returns null if the queue is full
-        // returns pointer to the node if there is space
-        // returned pointer can be used to delete the node
-        pub fn push(self: *Self, item: T) ?*Node(T) {
+        // returns index if there is space
+        // returned index can be used to perform remove
+        // returned index in valid until next pop or remove
+        pub fn push(self: *Self, item: T) ?usize {
 
             // if the queue is full, return false
             if (capacity == self.length) return null;
+
+            defer self.length += 1;
 
             // use the next available slot in items as new node
             var new_node_ptr = &self.items[self.length];
@@ -46,10 +50,11 @@ pub fn FixedQueue (
             // the new node becomes the new tail
             self.tail_ptr = new_node_ptr;
 
-            self.length += 1;
-            return new_node_ptr;
+            return self.length;
         }
 
+        // TODO: Use a Circular Buffer to implement a fixed size queue
+        // TODO: overhaul
         // remove and return the first item in the queue, if exists
         pub fn pop(self: *Self) ?T {
 
@@ -65,63 +70,119 @@ pub fn FixedQueue (
             };
             right_ptr.left_ptr = null;
 
-            // quick removal of head
+            // get the result before it gets overwritten
+            const result = head_ptr.value;
+
+            self.removeNode(head_ptr);
+            return result;
+        }
+
+        pub fn removeNode(self: *Self, node: *Node(T)) void {
+            // connects the left and right nodes
+            if (node.left_ptr) | left_ptr | {
+                left_ptr.right_ptr = node.right_ptr;
+            }
+            if (node.right_ptr) | right_ptr | {
+                right_ptr.left_ptr = node.left_ptr;
+            }
+
+            // check if the node is the head or tail
+            if (node == self.tail_ptr) {
+                self.tail_ptr = node.left_ptr;
+            }
+            if (node == self.head_ptr) {
+                self.head_ptr = node.right_ptr;
+            }
+
+            // quick removal of node
             // steps:
             // get the last node in the list
             // copy contents of the last node to the head
             // decrease the length
-            const last_index = self.length - 1;
-            var last_node_ptr = &self.items[last_index];
-
-            // get the result before it gets overwritten
-            const result = head_ptr.value;
+            var last_node_ptr = &self.items[self.length - 1];
 
             // update the left and right of the node
             // so that they refer to the newly moved node
             if (last_node_ptr.left_ptr) | last_left_ptr | {
-                last_left_ptr.right_ptr = head_ptr;
+                last_left_ptr.right_ptr = node;
             }
             if (last_node_ptr.right_ptr) | last_right_ptr | {
-                last_right_ptr.left_ptr = head_ptr;
+                last_right_ptr.left_ptr = node;
             }
+
             // if the tail is the last node, update the tail
             if (self.tail_ptr.? == last_node_ptr) {
-                self.tail_ptr = head_ptr;
+                self.tail_ptr = node;
+            }
+            if (self.head_ptr.? == last_node_ptr) {
+                self.head_ptr = node;
             }
 
             // replace the head with the last node
-            head_ptr.value = last_node_ptr.value;
-            head_ptr.left_ptr = last_node_ptr.left_ptr;
-            head_ptr.right_ptr = last_node_ptr.right_ptr;
+            node.value = last_node_ptr.value;
+            node.left_ptr = last_node_ptr.left_ptr;
+            node.right_ptr = last_node_ptr.right_ptr;
 
             // clean up the last_node_ptr
             last_node_ptr.left_ptr = null;
             last_node_ptr.right_ptr = null;
 
-            // the elem becomes the new head
-            // if it is not the same node as swapped
-            if (right_ptr != last_node_ptr) {
-                self.head_ptr = right_ptr;
-            }
-
             self.length -= 1;
-            return result;
         }
 
-        // n must exist in this linked list as a valid node
-        // pub fn remove(self: Self, n: *Node(T)) bool {
-        //     // link the left and the right
-        //     if (n.left_ptr) |*left_ptr| {
-        //         left_ptr.*.right_ptr = n.left_ptr;
-        //     }
-        //     if (n.right_ptr) |*right_ptr| {
-        //         right_ptr.*.left_ptr = n.right_ptr;
-        //     }
+        // index must exist in this linked list as a valid node
+        // calling remove or pop consecutively invalidates the index
+        pub fn remove(self: *Self, index: usize) void {
 
-        //     // TODO: remove from the list
-        //     // reduce the length
+            // link the left and the right
+            var n = &self.items[index];
+            if (n.left_ptr) | left_ptr | {
+                left_ptr.right_ptr = n.right_ptr;
+            }
+            if (n.right_ptr) | right_ptr | {
+                right_ptr.left_ptr = n.left_ptr;
+            }
 
-        // }
+            // copy from last node
+            const last_index = self.length - 1;
+            var last_node_ptr = &self.items[last_index];
+            n.left_ptr = last_node_ptr.left_ptr;
+            n.right_ptr = last_node_ptr.right_ptr;
+            n.value = last_node_ptr.value;
+
+            if (k) self.debugPrint();
+
+            // update the left and right of the last_node
+            // so that they refer to the moved node
+            if (last_node_ptr.left_ptr) | last_left_ptr | {
+                last_left_ptr.right_ptr = n;
+            }
+            if (last_node_ptr.right_ptr) | last_right_ptr | {
+                last_right_ptr.left_ptr = n;
+            }
+
+            // if the tail is the last node, update the tail
+            if (self.tail_ptr) |tail_ptr| {
+                if (tail_ptr == last_node_ptr) {
+                    if (k) warn("removed node is a tail",.{});
+                    self.tail_ptr = n;
+                }
+            }
+
+            if (self.head_ptr) |head_ptr| {
+                if (head_ptr == last_node_ptr) {
+                    if (k) warn("removed node is a head",.{});
+                    self.head_ptr = n;
+                }
+            }
+            // if the head is the last node, update the head
+
+            // invalidates current node
+            last_node_ptr.left_ptr = null;  
+            last_node_ptr.right_ptr = null;
+
+            self.length -= 1;
+        }
 
         fn debugPrint(self: Self) void {
             warn("\n LEN:{}",.{self.length});
@@ -194,9 +255,8 @@ test "FixedQueue" {
     }
 }
   
-test "FixedQueue" {
+test "FixedQueue - 1" {
     var queue = FixedQueue(u32, 5){};
-    _ = queue;
     {
         try testing.expect(null != queue.push(10));
         try testing.expectEqual(@as(?u32, 10), queue.pop() orelse unreachable);
@@ -218,3 +278,44 @@ test "FixedQueue" {
         try testing.expectEqual(@as(?u32, null), queue.pop());
     }
 }
+
+// test "FixedQueue - 2" {
+//     var queue = FixedQueue(u32, 5){};
+//     {
+//         try testing.expect(@as(usize, 0) == queue.push(10));
+//         try testing.expect(@as(usize, 1) == queue.push(11));
+//         try testing.expect(@as(usize, 2) == queue.push(12));
+// 
+//         queue.remove(1);
+//         try testing.expectEqual(@as(?u32, 10), queue.pop() orelse unreachable);
+//         try testing.expectEqual(@as(?u32, 12), queue.pop() orelse unreachable);
+//     }
+// }
+// 
+// test "FixedQueue - 2" {
+//     var queue = FixedQueue(u32, 5){};
+//     {
+//         try testing.expect(@as(usize, 0) == queue.push(10));
+//         try testing.expect(@as(usize, 1) == queue.push(11));
+//         try testing.expect(@as(usize, 2) == queue.push(12));
+// 
+//         queue.remove(2);
+//         try testing.expectEqual(@as(?u32, 10), queue.pop() orelse unreachable);
+//         try testing.expectEqual(@as(?u32, 11), queue.pop() orelse unreachable);
+//     }
+// }
+// 
+// test "FixedQueue - 3" {
+//     var queue = FixedQueue(u32, 5){};
+//     {
+//         try testing.expect(@as(usize, 0) == queue.push(10));
+//         try testing.expect(@as(usize, 1) == queue.push(11));
+//         try testing.expect(@as(usize, 2) == queue.push(12));
+// 
+//         k = true;
+//         queue.remove(0);
+// 
+//         try testing.expectEqual(@as(?u32, 11), queue.pop() orelse unreachable);
+//         try testing.expectEqual(@as(?u32, 12), queue.pop() orelse unreachable);
+//     }
+// }
