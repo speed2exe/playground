d71d49bcb4b0e0c80b10801bb40d457339eb2bd5
Fu Zi Xiang <zixiang.fu@bybit.com>
2022-09-19 14:26:40 +0800

    change linux to c in termios_c.zig

--- zig/icli/termios_c.zig
+++ zig/icli/termios_c.zig
@@ -1,18 +1,17 @@
 const std = @import("std");
-const linux = std.os.linux;
 const c = std.c;
 const File = std.fs.File;
 
 // based on: https://www.gnu.org/software/libc/manual/html_node/Noncanonical-Input.html
 pub fn getRawModeTermios(termios: std.os.termios) std.os.termios {
     var raw_mode = termios;
-    raw_mode.iflag &= ~(linux.IGNBRK | linux.BRKINT | linux.PARMRK | linux.ISTRIP | linux.INLCR | linux.IGNCR | linux.ICRNL | linux.IXON);
-    raw_mode.oflag &= ~linux.OPOST;
-    raw_mode.lflag &= ~(linux.ECHO | linux.ECHONL | linux.ICANON | linux.ISIG | linux.IEXTEN);
-    raw_mode.cflag &= ~(linux.CSIZE | linux.PARENB);
-    raw_mode.cflag |= linux.CS8;
-    raw_mode.cc[linux.V.MIN] = 1;
-    raw_mode.cc[linux.V.TIME] = 0;
+    raw_mode.iflag &= ~(c.IGNBRK | c.BRKINT | c.PARMRK | c.ISTRIP | c.INLCR | c.IGNCR | c.ICRNL | c.IXON);
+    raw_mode.oflag &= ~c.OPOST;
+    raw_mode.lflag &= ~(c.ECHO | c.ECHONL | c.ICANON | c.ISIG | c.IEXTEN);
+    raw_mode.cflag &= ~(c.CSIZE | c.PARENB);
+    raw_mode.cflag |= c.CS8;
+    raw_mode.cc[c.V.MIN] = 1;
+    raw_mode.cc[c.V.TIME] = 0;
     return raw_mode;
 }
 
--- zig/icli/test.zig
+++ zig/icli/test.zig
@@ -1,8 +1,11 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 pub fn main() void {
-    var s = S{};
-    s.f(u8, 1);
+    std.debug.print("tag: {any}", .{builtin.os.tag});
+
+    // var s = S{};
+    // s.f(u8, 1);
 }
 
 const S = struct {
