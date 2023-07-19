354ee11d22f1d438bd0469c257980a45768376b5
Fu Zi Xiang <speed@Fus-Laptop.local>
2023-07-19 09:22:30 +0800

    rust: mysql: add some example

--- rust/hello/src/main.rs
+++ rust/hello/src/main.rs
@@ -1,3 +1,30 @@
+use std::borrow::Borrow;
+
 fn main() {
-    println!("Hello, world!");
+    let my_struct = MyStruct {
+        id: 1,
+        name: "ho".to_string(),
+    };
+
+    println!("my_struct: {:?}", my_struct);
+    some_func(&my_struct);
+    some_func_2(my_struct);
+}
+
+fn some_func(s: &MyStruct) {
+    println!("s: {:?}", s);
+}
+
+fn some_func_2<B>(s: B)
+where
+    B: Borrow<MyStruct> + std::fmt::Debug,
+{
+    println!("s: {:?}", s);
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+struct MyStruct {
+    id: i32,
+    name: String,
 }
--- rust/mysql_test/src/main.rs
+++ rust/mysql_test/src/main.rs
@@ -1,8 +1,58 @@
+use std::borrow::Borrow;
+
+use mysql::prelude::FromRow;
+use mysql::prelude::Queryable;
+use mysql::FromRowError;
 use mysql::Pool;
+
 fn main() {
     let url = "mysql://root@localhost:3306";
     let pool = Pool::new(url).unwrap();
-    println!("pool: {:?}", pool);
-    let mut conn = pool.get_conn().unwrap();
-    println!("conn: {:?}", conn);
+    let conn = pool.get_conn().unwrap();
+    let mut conn2 = conn.unwrap();
+
+    {
+        let result: Vec<bool> = conn2
+            .exec::<bool, &str, ()>("CREATE DATABASE IF NOT EXISTS test3", ())
+            .unwrap();
+        println!("{:?}", result);
+    }
+    {
+        conn2
+            .exec::<bool, &str, ()>(
+                "CREATE TABLE IF NOT EXISTS test.users (id int, name varchar(255))",
+                (),
+            )
+            .unwrap();
+    }
+    {}
+    {
+        let result = conn2.query::<Person, _>("select id, name from test.user");
+        println!("{:?}", result);
+    }
+    {
+        let result = conn2.query_map("select id, name from test.user", |(id, name)| Person {
+            id,
+            name,
+        });
+        println!("{:?}", result);
+    }
+    {}
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct Person {
+    id: i32,
+    name: String,
+}
+
+impl FromRow for Person {
+    fn from_row_opt(row: mysql::Row) -> Result<Person, FromRowError>
+    where
+        Person: Sized,
+    {
+        println!("hello");
+        let (id, name) = mysql::from_row(row);
+        Ok(Person { id, name })
+    }
 }
